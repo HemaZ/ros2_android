e1ec04fda8a0aeb7256397bb068299954ed10485
Ibrahim Essam <ibrahim.essam1995@gmail.com>
2020-07-14 01:01:10 +0200

    update Readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ros2_android
-ROS2 example packages to use your android device sensors
+ROS2 example packages to use your android device sensors and camera.
+
+Download this apps on your Android device [CamON Live Streaming‏](https://play.google.com/store/apps/details?id=com.spynet.camon&hl=ar).
+
+Then You can run the ros node as follow
+
+`ros2 run android_node android --ros-args -p "ip:=DEVICE_IP" -p port:=PORT -p fps:=15 -p gps_freq:=2`
+
+replace **DEVICE_IP** and **PORT** with Your Device IP and Port number from the app.
+
+### optional parameters:
+
+* fps : camera topic rate Default:15
+* gps_freq : GPS topic rate Default:2
+
+### Available services: 
+| Service name  | Type                             | Request     |
+|---------------|----------------------------------|-------------|
+| /camera_zoom  | android_node_interfaces.srv.Zoom | [-100,100]  |
+| /toggle_torch | android_node_interfaces.srv.Torc | True, False |
+
+### Build status:
+
+![Test CI](https://github.com/HemaZ/ros2_android/workflows/Test%20CI/badge.svg)
--- android_node/android_node/android_node.py
+++ android_node/android_node/android_node.py
@@ -1,55 +1,73 @@
 import json
 import urllib.request
-
 import cv2
 import numpy as np
 import rclpy
 from cv_bridge import CvBridge
 from rclpy.node import Node
+from rclpy.exceptions import ParameterNotDeclaredException
+from rcl_interfaces.msg import ParameterType, ParameterDescriptor
 from sensor_msgs.msg import Image, NavSatFix
-
 from android_node_interfaces.srv import Torch, Zoom
 
 
 class Android(Node):
-    def __init__(self, ip, port=8080):
-        self.ip_ = ip
-        self.port_ = port
-        self.url_ = "http://" + ip + ":" + str(port) + "/"
-        self.update_status_()
-        self.model_ = self.status['model'].replace("-", "_")
-        super().__init__(self.model_)
+    def __init__(self):
+        super().__init__("android_node")
+        self.declare_parameter('ip',
+                               '192.168.1.122')
+        self.declare_parameter('port', 8080)
+        self.declare_parameter('fps', 15)
+        self.declare_parameter('gps_freq', 2)
+        self.ip_ = self.get_parameter(
+            'ip').get_parameter_value().string_value
+        self.port_ = self.get_parameter(
+            'port').get_parameter_value().integer_value
+        self.url_ = 'http://' + self.ip_ + ':' + str(self.port_) + '/'
+        self.fps_ = self.get_parameter(
+            'fps').get_parameter_value().integer_value
+        self.gps_freq_ = self.get_parameter(
+            'gps_freq').get_parameter_value().integer_value
+        try:
+            self.update_status_()
+        except urllib.error.URLError:
+            self.get_logger().error('Can not Connect to the url {}'.format(self.url_))
+            return
+
+        self.model_ = self.status['model'].replace('-', '_')
         self.camera_pub_ = self.create_publisher(
-            Image, self.model_ + "/camera", 1)
+            Image, self.model_ + '/camera', 1)
         self.gps_pub_ = self.create_publisher(
-            NavSatFix, self.model_ + "/gps", 1)
+            NavSatFix, self.model_ + '/gps', 1)
         self.cv_bridge_ = CvBridge()
-        self.timer_camera = self.create_timer(1 / 15, self.timer_camera_clk_)
-        self.timer_gps = self.create_timer(0.5, self.timer_gps_clk_)
+        self.timer_camera = self.create_timer(
+            1/self.fps_, self.timer_camera_clk_)
+        self.timer_gps = self.create_timer(
+            1/self.gps_freq_, self.timer_gps_clk_)
         self.torch_srv_ = self.create_service(
-            Torch, "/{}/toggle_torch".format(self.model_), self.torch_srv_clk_)
+            Torch, '/{}/toggle_torch'.format(self.model_), self.torch_srv_clk_)
         self.zoom_srv_ = self.create_service(
-            Zoom, "/{}/camera_zoom".format(self.model_), self.zoom_srv_clk_)
+            Zoom, '/{}/camera_zoom'.format(self.model_), self.zoom_srv_clk_)
 
     def update_status_(self):
-        with urllib.request.urlopen(self.url_ + "status") as url:
+        with urllib.request.urlopen(self.url_ + 'status') as url:
             self.status = json.loads(url.read().decode())
 
     def update_sensors_(self):
-        with urllib.request.urlopen(self.url_ + "sensors") as url:
+        with urllib.request.urlopen(self.url_ + 'sensors') as url:
             self.sensors = json.loads(url.read().decode())
 
     def timer_camera_clk_(self):
         try:
-            resp = urllib.request.urlopen(self.url_ + "jpeg")
-            image = np.asarray(bytearray(resp.read()), dtype="uint8")
+            resp = urllib.request.urlopen(self.url_ + 'jpeg')
+            image = np.asarray(bytearray(resp.read()), dtype='uint8')
             image = cv2.imdecode(image, cv2.IMREAD_COLOR)
             img_msg = self.cv_bridge_.cv2_to_imgmsg(image)
             img_msg.header.frame_id = self.model_
             img_msg.header.stamp = self.get_clock().now().to_msg()
             self.camera_pub_.publish(img_msg)
         except BaseException:
-            self.get_logger().error("Can't Connect to the device {}".format(self.model_))
+            self.get_logger().error('Can not Connect to the device {}'.format(self.model_))
             self.destroy_node()
 
     def timer_gps_clk_(self):
@@ -58,33 +76,33 @@ class Android(Node):
         msg.latitude = self.status['location']['latitude']
         msg.longitude = self.status['location']['longitude']
         msg.position_covariance_type = 0
-        msg.header.frame_id = "gps"
+        msg.header.frame_id = 'gps'
         self.gps_pub_.publish(msg)
 
     def torch_srv_clk_(self, request, response):
-        self.get_logger().info("Got torch toggling request {}".format(request.state))
+        self.get_logger().info('Got torch toggling request {}'.format(request.state))
         if request.state:
-            resp = urllib.request.urlopen(self.url_ + "control?torch=on")
+            resp = urllib.request.urlopen(self.url_ + 'control?torch=on')
         else:
-            resp = urllib.request.urlopen(self.url_ + "control?torch=off")
+            resp = urllib.request.urlopen(self.url_ + 'control?torch=off')
         self.update_sensors_()
-        if self.sensors['torch'] == "false":
+        if self.sensors['torch'] == 'false':
             response.response = False
         else:
             response.response = True
         return response
 
     def zoom_srv_clk_(self, request, response):
-        self.get_logger().info("Got Zoom request {}".format(request.delta))
+        self.get_logger().info('Got Zoom request {}'.format(request.delta))
         try:
             if int(request.delta) > 0:
-                resp = urllib.request.urlopen(
-                    self.url_ + "control?zoom=+{}".format(int(request.delta)))
-                print("control?zoom=+{}".format(int(request.delta)))
+                _ = urllib.request.urlopen(
+                    self.url_ + 'control?zoom=+{}'.format(int(request.delta)))
+                print('control?zoom=+{}'.format(int(request.delta)))
             else:
-                resp = urllib.request.urlopen(
-                    self.url_ + "control?zoom={}".format(int(request.delta)))
-                print("control?zoom={}".format(int(request.delta)))
+                _ = urllib.request.urlopen(
+                    self.url_ + 'control?zoom={}'.format(int(request.delta)))
+                print('control?zoom={}'.format(int(request.delta)))
             status = True
         except BaseException:
             status = False
@@ -94,11 +112,11 @@ class Android(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    device = Android("192.168.1.122")
+    device = Android()
     rclpy.spin(device)
     device.destroy_node()
     rclpy.shutdown()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
